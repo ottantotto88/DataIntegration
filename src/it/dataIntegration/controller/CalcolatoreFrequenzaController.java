1d491f828b74eafcf769ae3293c81e079a55f185
ottantotto88 <ottantotto88>
2018-11-11 13:36:35 +0100

    filtraggio link alle notizie completo

--- src/it/dataIntegration/controller/CalcolatoreFrequenzaController.java
+++ src/it/dataIntegration/controller/CalcolatoreFrequenzaController.java
@@ -10,17 +10,21 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import it.dataIntegration.view.CalcolatoreFrequenzaView1;
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.jsoup.Connection.Response;
 
 
 public class CalcolatoreFrequenzaController {
 
     private CalcolatoreFrequenzaView1 calcolatoreFrequenzaView1;
     private ArrayList<DbpediaObject> dbpediaObjects = new ArrayList<DbpediaObject>();
+    private ArrayList<String> news = new ArrayList<>();
     private Document document;
+    private Response response;
     private String argomento;
 
 
@@ -43,26 +47,34 @@ public class CalcolatoreFrequenzaController {
             public void actionPerformed(ActionEvent e) {
                 String argomento = calcolatoreFrequenzaView1.getArgomentiTextField().getText();
                 try {
-                document= Jsoup.connect("https://news.google.com/search?q="+ argomento +"&hl=it&gl=IT&ceid=IT%3Ait").get();
+                    document= Jsoup.connect("https://news.google.com/search?q="+ argomento +"&hl=it&gl=IT&ceid=IT%3Ait").get();
+                    Elements links = document.select("a[href]");
+
+                    news = filtraNotizie(links);
+                    System.out.println(news.get(1));
+                    System.out.println(Jsoup.connect(news.get(1)).followRedirects(true).execute().header("location"));
                 } catch (IOException exception) {
                 //blocco di catch autogenerato per la gestione eccezione di jsoup
-                exception.printStackTrace();
+                    exception.printStackTrace();
                 }
 
-                Elements links = document.select("a[href]");
-
-                System.out.println("\nExtracted keywords:");
-                for (Element link : links) {
-                    String url = link.attr("abs:href");
-                    dbpediaObjects = RestServices.getRequest(url);
-                    for (DbpediaObject dbpediaObject : dbpediaObjects){
-                        System.out.println(dbpediaObject.getUriDbpedia());
-
-                    }
-                }
             }
         });
 
 
+    }
+
+    //questo metodo prende in ingresso i risultati del parser html, che carica tutti i link che trova nella pagina.
+    //Ai fini della ricerca sono ovviamente interessanti tutti e soli i link inerenti alle notizie (si vogliono ignorare
+    // i link ad esempioa google play o altri servizi). Vengono quindi filtrati e restituiti come una lista di stringhe.
+    public ArrayList<String> filtraNotizie(Elements links){
+        ArrayList<String> notizie = new ArrayList<>();
+
+        for (Element link : links) {
+            String url = link.attr("abs:href");
+            if(url.matches("https://news.google.com/article(.*)"))
+                notizie.add(url);
+        }
+        return notizie;
     }
 }
